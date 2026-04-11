f7181b296b8b76998128d9dc423ed518e1e8a327
SilverRuler <SilverRuler@github.com>
2026-04-11 18:23:04 +0900

    feat: add android support using capacitor

---
+++ android/app/src/main/java/com/silverruler/pokestickerbook/MainActivity.java
@@ -0,0 +1,5 @@
+package com.silverruler.pokestickerbook;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.silverruler.pokestickerbook',
+  appName: 'pokemon-collector',
+  webDir: 'dist'
+};
+
+export default config;
